fca4ecba8a6a67a6916a71b2f8c15174ca5d00ed
Evan Hildreth <slightly@oddevan.com>
2015-02-05 17:51:58 -0500

    Changing to command line app
    Let’s not introduce SpriteKit just yet.

---
+++ macguffin/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macguffin
+//
+//  Created by Evan Hildreth on 2/5/15.
+//  Copyright (c) 2015 Evan Hildreth. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
