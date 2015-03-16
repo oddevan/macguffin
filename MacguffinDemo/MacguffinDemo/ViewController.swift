99a98f671f6707464355d618aae93b1d3aed6b30
Evan Hildreth <slightly@oddevan.com>
2015-03-15 20:17:17 -0400

    Initial attempt

---
+++ MacguffinDemo/MacguffinDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacguffinDemo
+//
+//  Created by Evan Hildreth on 3/15/15.
+//  Copyright (c) 2015 Evan Hildreth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
